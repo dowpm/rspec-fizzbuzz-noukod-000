a45df3cc2260e21449fa80e0939723b466727f39
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-30 18:44:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-  if number % 3 == 0
-    if number % 5 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
+    if int % 5 == 0
       "FrizzBuzz"
     else
       "Frizz"
     end
-  elsif number % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   else
     nil
